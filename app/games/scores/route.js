a52e6f23215ab2710e38a0d252ba628b0a514906
Ryan Tablada <ryan.tablada@gmail.com>
2016-02-16 23:16:11 -0600

    10

--- app/games/scores/route.js
+++ app/games/scores/route.js
@@ -1,20 +1,4 @@
 import Ember from 'ember';
-const { hash, all } = Ember.RSVP;
 
 export default Ember.Route.extend({
-  model(params) {
-    return fetch(`https://game-scores.herokuapp.com/games/${params.gameId}`)
-      .then((res) => res.json())
-      .then((res) => {
-        const scores = res.data.relationships.scores.data;
-        return hash({
-          game: res.data,
-          scores: all(scores.map((score) => {
-            return fetch(`https://game-scores.herokuapp.com/game-scores/${score.id}`)
-              .then((res) => res.json())
-              .then((res) => res.data);
-          })),
-        });
-      });
-  }
 });
